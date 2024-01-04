27d0ed96fcad39475ed6fcd5c371946f47522282
kulwinder singh <kulwinders.kvbrgh@gmail.com>
2024-01-04 22:19:12 +0530

    Add files via upload

---
+++ src/Assets/data.js
@@ -0,0 +1,107 @@
+// data of all the product's in home page
+// id, name, image url , price and category of the product
+
+
+export const data = [
+    {
+        id:"1",
+        name:"AUSK T-Shirt for Mens Double Stripes on Chest",
+        image:"https://m.media-amazon.com/images/I/81wFLUDzAbL._SL1500_.jpg",
+        price:199,
+        category:"men"
+
+    },
+    {
+        id:"2",
+        name:"Men's Striped Regular fit T-Shirt ",
+        image:"https://m.media-amazon.com/images/I/51oibg+3wtL._SX569_.jpg",
+        price:247,
+        category:"men"
+
+    },
+    {
+        id:"3",
+        name:"American Tourister Valex 28 Ltrs Large Laptop Backpack",
+        image:"https://m.media-amazon.com/images/I/913C9PlkyfL._SX679_.jpg",
+        price:949,
+        category:"men"
+    },
+    {
+        id:"4",
+        name:"Philips HD9316/06 1.7-Liter Electric Kettle",
+        image:"https://m.media-amazon.com/images/I/71JZ7N7dO5L._SX466_.jpg",
+        price:2089,
+        category:"electric"
+    },
+    {
+        id:"5",
+        name:"Arrow Men's Solid Regular Pants",
+        image:"https://m.media-amazon.com/images/I/61c8vmA20GL._SX569_.jpg",
+        price:1099,
+        category:"men"
+    },
+    {
+        id:"6",
+        name:"Lakme Blush & Glow Kiwi Freshness Gel Face Wash",
+        image:"https://m.media-amazon.com/images/I/51a7eIQH5DL._SY450_.jpg",
+        price:199,
+        category:"women"
+    },
+    {
+        id:"7",
+        name:"Noise Twist Bluetooth Calling Smart Watch ",
+        image:"https://m.media-amazon.com/images/I/61TapeOXotL._SX679_.jpg",
+        price:2199,
+        category:"electric"
+    },
+    {
+        id:"8",
+        name:"Dell 15 Laptop, Intel Core i5-1135G7 Processor/8GB DDR4/512GB/Intel UHD Graphic/15.6",
+        image:"https://m.media-amazon.com/images/I/41HxuKnAu-L._SX300_SY300_QL70_FMwebp_.jpg",
+        price:46999,
+        category:"electric"
+    },
+    {
+        id:"9",
+        name:"Shasmi Women's Georgette Digital Floral Printed Gown Dress",
+        image:"https://m.media-amazon.com/images/I/71GxB8DJLyL._SY741_.jpg",
+        price:799,
+        category:"women"
+    },
+    {
+        id:"10",
+        name:" Women's Sweatshirt 'M' size Pink ",
+        image:"https://m.media-amazon.com/images/I/71eq6q3uVeL._AC_UY1100_.jpg",
+        price:3499,
+        category:"women"
+    },
+    {
+        id:"11",
+        name:"Gold Jewellery Set necklace and earrings",
+        image:"https://m.media-amazon.com/images/I/41L6gVqjhQL._AC_UL400_.jpg",
+        price:25499,
+        category:"jewellery"
+    },
+    {
+        id:"12",
+        name:"Women Leather Bag",
+        image:"https://m.media-amazon.com/images/I/71Y32csE9vL._UY695_.jpg",
+        price:4449,
+        category:"women"
+    },
+    {
+        id:"13",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"14",
+        name:"HP Wired Keyboard",
+        image:"https://m.media-amazon.com/images/I/31-AngJwVkL._SX300_SY300_QL70_FMwebp_.jpg",
+        price:599,
+        category:"electric"
+    }
+
+];
